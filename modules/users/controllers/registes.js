27bad34edbbf903f6ea815edf9dc9891fd380c26
wandi1209 <wandider@gmail.com>
2025-02-22 19:38:31 +0700

    create exception for duplicate email

--- modules/users/controllers/registes.js
+++ modules/users/controllers/registes.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
+const userModel = require("../../../models/user");
 
 const register = async (req, res) => {
   const usersModel = mongoose.model("users");
 
   const { name, email, password, confirm_password, balance } = req.body;
+
+  const getDuplicateEmail = await userModel.findOne({
+    email: email,
+  });
+
+  if (getDuplicateEmail) throw "This email already exists!";
 
   await usersModel.create({
     name: name,
