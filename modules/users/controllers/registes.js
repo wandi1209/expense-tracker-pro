928cb40c2d66bf45993f1b7e2e8922543866e115
wandi1209 <wandider@gmail.com>
2025-02-22 19:22:41 +0700

    created register controller

--- modules/users/controllers/registes.js
+++ modules/users/controllers/registes.js
@@ -1,6 +1,19 @@
-const register = (req, res) => {
-  res.status(200).json({
-    status: "Hello from register",
+const mongoose = require("mongoose");
+
+const register = async (req, res) => {
+  const usersModel = mongoose.model("users");
+
+  const { name, email, password, confirm_password, balance } = req.body;
+
+  await usersModel.create({
+    name: name,
+    email: email,
+    password: password,
+    balance: balance,
+  });
+  res.status(201).json({
+    status: "success",
+    message: "User registered successfully",
   });
 };
 
