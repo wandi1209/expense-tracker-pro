558e52317377fc5b96449ef5a933640f89a55d22
wandi1209 <wandider@gmail.com>
2025-02-22 19:47:54 +0700

    create another error handling for register

--- modules/users/controllers/registes.js
+++ modules/users/controllers/registes.js
@@ -6,6 +6,15 @@ const register = async (req, res) => {
 
   const { name, email, password, confirm_password, balance } = req.body;
 
+  // Validation
+  if (!email) throw "Email must be provided!";
+  if (!password) throw "Password must be provided!";
+  if (password.length < 5) throw "Password must be at least 5 characters long.";
+
+  if (!name) throw "Name must be provided!";
+  if (password !== confirm_password)
+    throw "Password and confirmed password doesn't match!";
+
   const getDuplicateEmail = await userModel.findOne({
     email: email,
   });
