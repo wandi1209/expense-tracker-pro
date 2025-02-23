43c601ab333dd4275d614cb6181c6cfd6fc9832c
wandi1209 <wandider@gmail.com>
2025-02-23 21:47:50 +0700

    create user route for dashboard

---
+++ modules/users/controllers/userDashboard.js
@@ -0,0 +1,7 @@
+const userDashboard = async (req, res) => {
+  res.status(200).json({
+    status: "Hello from  userRegister",
+  });
+};
+
+module.exports = userDashboard;
--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const register = require("./controllers/registes");
 const login = require("./controllers/login");
+const userDashboard = require("./controllers/userDashboard");
 
 const userRoute = express.Router();
 
 // Routes...
 userRoute.post("/register", register);
 userRoute.post("/login", login);
+userRoute.get("/dashboard", userDashboard);
 
 module.exports = userRoute;
