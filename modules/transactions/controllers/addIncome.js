a82b352ab64a00609ed0043087366eae362dd5aa
wandi1209 <wandider@gmail.com>
2025-02-26 15:51:26 +0700

    create logic to add income and increase balance user

--- modules/transactions/controllers/addIncome.js
+++ modules/transactions/controllers/addIncome.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const addIncome = async (req, res) => {
   const userModel = mongoose.model("users");
@@ -9,6 +10,29 @@ const addIncome = async (req, res) => {
   if (!amount) throw "Amount is required!";
   if (!remarks) throw "Remarks is required!";
   if (remarks.length < 5) throw "Remarks must be at least 5 characters long!";
+  if (!validator.isNumeric(amount.toString()))
+    throw "Amount must be a valid number";
+
+  await transactionsModel.create({
+    user_id: req.user._id,
+    amount: amount,
+    remarks: remarks,
+    transation_field: "income",
+  });
+
+  await userModel.updateOne(
+    {
+      _id: req.user._id,
+    },
+    {
+      $inc: {
+        balance: amount,
+      },
+    },
+    {
+      runValidators: true,
+    }
+  );
 
   res.status(200).json({
     status: "success",
