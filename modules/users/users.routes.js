57487355f82a3624cc183ccd046296146af1f480
wandi1209 <wandider@gmail.com>
2025-05-16 07:10:35 +0700

    add route logout

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -5,15 +5,17 @@ const userDashboard = require("./controllers/userDashboard");
 const auth = require("../../middleware/auth");
 const forgotPassword = require("./controllers/forgotPassword");
 const resetPassword = require("./controllers/resetPassword");
+const logout = require("./controllers/logout");
 
 const userRoute = express.Router();
 
 // Routes...
 userRoute.post("/register", register);
 userRoute.post("/login", login);
+userRoute.post("/logout", logout);
 
-userRoute.post("/forgot-password", forgotPassword)
-userRoute.post("/reset-password", resetPassword)
+userRoute.post("/forgot-password", forgotPassword);
+userRoute.post("/reset-password", resetPassword);
 
 userRoute.use(auth);
 
